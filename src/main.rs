eb757a709cc09fff33096fb423b2c757549fedc3
drakeerv <drakeerv@outlook.com>
2025-02-09 10:37:03 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,19 @@ struct IntakeMarker;
 #[derive(Resource, Default)]
 struct MouseWorldPos(Vec2);
 
+#[derive(Resource, Default)]
+enum ControlMode {
+    #[default]
+    CodeControl,
+    CursorFollow,
+}
+
+#[derive(Resource)]
+struct MotorJoints {
+    elevator: Entity,
+    arm: Entity,
+}
+
 fn main() {
     App::new()
         .add_plugins((
@@ -18,8 +31,14 @@ fn main() {
             RapierDebugRenderPlugin::default(),
         ))
         .init_resource::<MouseWorldPos>()
+        .init_resource::<ControlMode>()
         .add_systems(Startup, (setup_graphics, setup_physics))
-        .add_systems(Update, (update_mouse_position, apply_intake_force))
+        .add_systems(Update, (
+            update_mouse_position,
+            apply_intake_force,
+            handle_control_mode,
+            update_motors,
+        ))
         .run();
 }
 
@@ -39,14 +58,68 @@ fn update_mouse_position(
     }
 }
 
+fn handle_control_mode(
+    keys: Res<ButtonInput<KeyCode>>,
+    mut control_mode: ResMut<ControlMode>,
+) {
+    if keys.just_pressed(KeyCode::Space) {
+        *control_mode = match *control_mode {
+            ControlMode::CodeControl => ControlMode::CursorFollow,
+            ControlMode::CursorFollow => ControlMode::CodeControl,
+        };
+        println!("Switched control mode");
+    }
+}
+
+fn update_motors(
+    control_mode: Res<ControlMode>,
+    mut joints: Query<&mut ImpulseJoint>,
+    motor_joints: Res<MotorJoints>,
+    keys: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+) {
+    if matches!(*control_mode, ControlMode::CodeControl) {
+        // Control elevator motor
+        if let Ok(mut joint) = joints.get_mut(motor_joints.elevator) {
+            let motor_speed = if keys.pressed(KeyCode::KeyW) {
+                1000.0
+            } else if keys.pressed(KeyCode::KeyS) {
+                -1000.0
+            } else {
+                0.0
+            };
+            joint.data.as_mut().set_motor_velocity(JointAxis::LinY, motor_speed, 1.0);
+        }
+
+        // Control arm motor
+        if let Ok(mut joint) = joints.get_mut(motor_joints.arm) {
+            let motor_speed = if keys.pressed(KeyCode::KeyD) {
+                5.0
+            } else if keys.pressed(KeyCode::KeyA) {
+                -5.0
+            } else {
+                0.0
+            };
+            joint.data.as_mut().set_motor_velocity(JointAxis::AngX, motor_speed, 1.0);
+        }
+    }
+}
+
 fn apply_intake_force(
+    control_mode: Res<ControlMode>,
     mouse_pos: Res<MouseWorldPos>,
     mut intake_query: Query<(&Transform, &mut ExternalForce), With<IntakeMarker>>,
 ) {
-    for (transform, mut ext_force) in intake_query.iter_mut() {
-        let direction = (mouse_pos.0 - transform.translation.truncate()).normalize();
-        ext_force.force = direction * 50000.0;
-        println!("Applying force: {:?}", ext_force.force);
+    if matches!(*control_mode, ControlMode::CursorFollow) {
+        for (transform, mut ext_force) in intake_query.iter_mut() {
+            let direction = (mouse_pos.0 - transform.translation.truncate()).normalize();
+            ext_force.force = direction * 50000.0;
+        }
+    } else {
+        // Reset forces when not in cursor follow mode
+        for (_, mut ext_force) in intake_query.iter_mut() {
+            ext_force.force = Vec2::ZERO;
+        }
     }
 }
 
@@ -138,11 +211,13 @@ fn setup_physics(mut commands: Commands) {
     let joint_elevator_carriage = PrismaticJointBuilder::new(Vec2::Y)
         .local_anchor1(Vec2::new(0.0, 0.0))
         .local_anchor2(Vec2::new(-5.08, 0.0))
-        .limits([-13.65, 31.75]);
+        .limits([-13.65, 31.75])
+        .motor_position(0.0, 1000.0, 100000.0); // Add motor to elevator
 
     let joint_carriage_arm = RevoluteJointBuilder::new()
         .local_anchor1(Vec2::new(0.0, 0.0))
-        .local_anchor2(Vec2::new(-19.685, 0.0));
+        .local_anchor2(Vec2::new(-19.685, 0.0))
+        .motor_position(0.0, 5.0, 100000.0); // Add motor to arm joint
 
     let joint_arm_pivot = RevoluteJointBuilder::new()
         .local_anchor1(Vec2::new(19.685, 0.0))
@@ -152,13 +227,21 @@ fn setup_physics(mut commands: Commands) {
         .local_anchor1(Vec2::new(0.0, 0.0))
         .local_anchor2(Vec2::new(0.0, -13.0));
 
-    commands
+    let elevator_joint = commands
         .spawn(ImpulseJoint::new(elevator_body, joint_elevator_carriage))
-        .set_parent(carriage);
+        .set_parent(carriage)
+        .id();
 
-    commands
+    let arm_joint = commands
         .spawn(ImpulseJoint::new(carriage, joint_carriage_arm))
-        .set_parent(arm);
+        .set_parent(arm)
+        .id();
+
+    // Store motor joint handles
+    commands.insert_resource(MotorJoints {
+        elevator: elevator_joint,
+        arm: arm_joint,
+    });
 
     commands
         .spawn(ImpulseJoint::new(arm, joint_arm_pivot))
